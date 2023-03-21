516fabce43089a63c780b4419c0a0b5102713027
Ardahan Celikkol <cardahan4@gmail.com>
2023-03-21 16:16:53 +0300

    Değişiklikler yapıldı

--- Program.cs
+++ Program.cs
@@ -6,7 +6,6 @@ public class Student
     public string Name { get; set; }
     public int Age { get; set; }
     public List<string> Courses { get; set; }
-    public bool IsGraduated { get; set; } // Yeni özellik
 
 
     public Student(string name, int age)
@@ -14,7 +13,6 @@ public class Student
         Name = name;
         Age = age;
         Courses = new List<string>();
-        IsGraduated = true; // Varsayılan değer olarak true atandı
     }
 
     public void AddCourse(string course)
@@ -41,12 +39,10 @@ public class Program
         Student alice = new Student("Alice", 20);
         alice.AddCourse("C# Programming");
         alice.AddCourse("Web Development");
-        alice.IsGraduated = false;
 
         Student bob = new Student("Bob", 22);
         bob.AddCourse("Java Programming");
         bob.AddCourse("Database Design");
-        bob.IsGraduated = true;
 
 
 
@@ -57,31 +53,6 @@ public class Program
         foreach (Student student in students)
         {
             student.PrintInfo();
-
-            if (student == bob)
-            {
-                if (student.IsGraduated)
-                {
-                    Console.WriteLine($"{student.Name} is graduated.");
-                }
-                else
-                {
-                    Console.WriteLine($"{student.Name} is not graduated yet.");
-                }
-            }
-
-            if (student == alice)
-            {
-                if (student.IsGraduated)
-                {
-                    Console.WriteLine($"{student.Name} is graduated.");
-                }
-                else
-                {
-                    Console.WriteLine($"{student.Name} is not graduated yet.");
-                }
-            }
-
             Console.WriteLine();
         }
     }
